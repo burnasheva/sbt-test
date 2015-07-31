50470d68c5b0a163c047ca296d8a22983406c521
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2015-07-31 17:22:15 +0300

    some tests are failing

---
+++ src/test/scala/FirstSpec2.scala
@@ -0,0 +1,16 @@
+import org.scalatest.FlatSpec
+import org.scalatest._
+import Matchers._
+
+class ExampleSpec2 extends FlatSpec {
+
+    "The Scala language" must "add correctly" in {
+      val sum = 1 + 1
+      assert(sum === 2)
+    }
+
+  it must "subtract correctly" in {
+    val diff = 4 - 1
+    assert(diff === 3)
+  }
+}
